c7b56507a9d39248a59f7a9cc90f186513006c3f
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-06-08 14:42:26 +0200

    image

---
+++ src/components/layout.js
@@ -0,0 +1,53 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import Socials from "../components/socials"
+import Language from "../components/language"
+
+import Header from "./header"
+import "./layout.css"
+import styled from "styled-components"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+     
+      <div style={{ fontFamily: 'Fjalla One' }}>
+        <Main>
+          <Socials></Socials>
+          <Language></Language>
+          {children}
+        </Main>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
+
+const Main = styled.main`
+{
+  color: var(--textColor);
+}`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,62 @@
 import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import styled from 'styled-components'
+
+import Layout from "../components/layout"
+//import Seo from "../components/seo"
+/*import NavBar from "../components/navbar"*/
+
+const IndexPage = () => (
+  <Layout>
+    <StaticImage
+      src="../images/homeImage.jpg"
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF", "JPG"]}
+      alt="Four Aces Belgica"
+      style={{ height: `100vh`, position: `absolute`, left: `0`, top: `0`, zIndex : `-1`, filter: `brightness(75%)` }}
+    />
+    <HomeTitle>FOUR ACES</HomeTitle>
+    <HomeSubTitle>Guitar Quartet</HomeSubTitle>
+    
+  </Layout>
+)
+
+//<Seo title="Home" />
+//<NavBar></NavBar>
+
+/*
+import Socials from "../components/socials"
+import Language from "../components/language"
+*/
+
+/*<p>
+  <Link to="/page-2/">Go to page 2</Link> <br />
+  <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+</p>*/
+
+export default IndexPage
+
+const HomeTitle = styled.div`
+  width: 100%;
+  font-weight: 1000;
+  text-align : center;
+  position: absolute;
+  font-size: 100px;
+  top: calc(40% - 100px);
+`
+
+const HomeSubTitle = styled.div`
+  width: 100%;
+  font-weight: 800;
+  text-align : center;
+  position: absolute;
+  font-size: 50px;
+  top: calc(45% - 50px);
+`
+
+
+/*import * as React from "react"
 
 // styles
 const pageStyles = {
@@ -182,3 +240,4 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+*/
